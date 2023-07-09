1dd0264dc25339ecc429fbc5d54bd958fda6a0c5
Surya Kumar <surya.kumar@klarna.com>
2023-07-09 13:13:35 +0200

    Refresh token functionality

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,35 +1,19 @@
 import {
   Body,
   Controller,
-  Get,
-  Param,
   Post,
-  Query,
   UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthGuard } from '@nestjs/passport';
 import { LoginDto } from './dto/auth.dto';
-// import { CustomerService } from 'src/customer/customer.service';
-
-// type User = {
-//   email: string;
-// };
-
-// @SkipAuth()
+import { RolesGuard } from 'src/role.guard';
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly authService: AuthService, // private readonly customerService: CustomerService,
   ) {}
 
-  // @Post('signup')
-  // async signup(@Body(new ValidationPipe()) loginDto: LoginDto): Promise<User> {
-  //   return this.authService.signup(loginDto);
-  // }
-
-  // @SkipAuth()
   @Post('login')
   async login(
     @Body(new ValidationPipe()) loginDto: LoginDto,
@@ -41,35 +25,11 @@ export class AuthController {
     return { token };
   }
 
-  // @Post('reset-password')
-  // async resetPassword(
-  //   @Body(new ValidationPipe()) loginDto: LoginDto,
-  // ): Promise<{ token: { accessToken: string; refreshToken: string } }> {
-  //   const token = await this.authService.login(
-  //     loginDto.email,
-  //     loginDto.password,
-  //   );
-  //   return { token };
-  // }
-
-  //   @Get('reset-password/:resetToken')
-  //   async forgotPasswordVerify(
-  //     @Param('resetToken') email: string,
-  //     @Query('code') code: string,
-  //   ): Promise<void> {
-  //     const user = await this.customerService.getCustomer({ email });
-  //     const isMatching = code === user[0].activationCode;
-  //   }
-
-  // @Post('refresh-token')
-  // @UseGuards(AuthGuard('jwt'))
-  // async refreshToken(
-  //   @Body() refreshTokenDto: { refreshToken: string },
-  // ): Promise<{ accessToken: string }> {
-  //   const { refreshToken } = refreshTokenDto;
-  //   const newAccessToken = await this.authService.refreshAccessToken(
-  //     refreshToken,
-  //   );
-  //   return newAccessToken;
-  // }
+  @Post('refresh-token')
+  @UseGuards(RolesGuard)
+  async refreshAccessToken(
+    @Body('refreshToken') refreshToken: string,
+  ): Promise<{ accessToken: string }> {
+    return await this.authService.refreshAccessToken(refreshToken);
+  }
 }
--- src/middlewares/authentication.middleware.ts
+++ src/middlewares/authentication.middleware.ts
@@ -1,7 +1,11 @@
-import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { CustomerService } from 'src/customer/customer.service';
-// import { AuthService } from './auth.service';
 import { JwtStrategy } from 'src/jwt.strategy';
 
 @Injectable()
@@ -15,20 +19,21 @@ export class AuthenticationMiddleware implements NestMiddleware {
 
   async use(req: Request, res: Response, next: NextFunction) {
     const token = req.headers.authorization?.split(' ')[1];
-    this.logger.log('Tokennn in middlware', req.headers.authorization);
-    if (token) {
-      try {
-        const decoded = await this.authService.verifyToken(token);
-        const user = await this.customerService.getCustomer({
-          id: decoded.userId,
-        });
+    if (!token) {
+      throw new UnauthorizedException();
+    }
 
-        this.logger.log('User info', user);
-        req['user'] = user; // Attach the user object to the request
-      } catch (error) {
-        // Handle token verification error
-        this.logger.error('Error Validating token', error);
-      }
+    try {
+      const decoded = await this.authService.verifyToken(token);
+      const user = await this.customerService.getCustomer({
+        id: decoded.userId,
+      });
+
+      this.logger.log('User info', user);
+      req['user'] = user; // Attach the user object to the request
+    } catch (error) {
+      // Handle token verification error
+      this.logger.error('Error Validating token', error);
     }
 
     next();
